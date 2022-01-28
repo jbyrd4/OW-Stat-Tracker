0c3148d31c519fb37e318b8cf1651e557547a013
jbyrd4 <jbyrd4@gmail.com>
2022-01-28 13:49:46 -0600

    winrates show dynamically

--- src/Components/Friends/Friend.js
+++ src/Components/Friends/Friend.js
@@ -3,6 +3,24 @@ import React, { useEffect, useState } from "react"
 
 
 export const Friend = ({friend}) => {
+    const [friendGames, setFriendGames] = useState([]);
+
+    useEffect(() => {
+      fetch("http://localhost:8088/friendGames?_expand=game&_expand=friend")
+        .then((res) => res.json())
+        .then((data) => {
+          setFriendGames(data);
+        });
+    }, []);
+
+    const winRate = () => {
+        const playedGames = friendGames.filter(game => game.friendId === friend.id)
+        const wonGames = playedGames.filter(game => game.game.result === true)
+        const winPercent = ((parseInt(wonGames.length) / parseInt(playedGames.length))*100).toFixed(2)
+        return winPercent
+    }
+
+
   return (
       <li>
         <div>Account: {friend.name}</div>
@@ -10,6 +28,7 @@ export const Friend = ({friend}) => {
         <div>DPS: {friend.dpsRank}</div>
         <div>Support: {friend.supRank}</div>
         <div>Notes: {friend.notes}</div>
+        <div>Winrate: {winRate()}%</div>
       </li>
   )
 }
